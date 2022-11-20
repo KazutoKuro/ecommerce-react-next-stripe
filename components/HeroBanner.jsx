494ed6dfc2672bdd372f694c624b80b6ce70b6c6
Zin Paing Soe <kuro@Zins-MacBook-Pro.local>
2022-11-20 18:11:35 +0630

    Updated hook up with sanity

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,32 +1,27 @@
 import React from "react";
 import Link from "next/link";
-// import { urlFor } from '../lib/client';
+import { urlFor } from "../lib/client";
 
 const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">{/* {heroBanner.smallText} */}SMALL TEXT</p>
-        <h3>{/* {heroBanner.midText} */} MID TEXT</h3>
-        <h1>{/* {heroBanner.largeText1} */} LARGE TEXT</h1>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
         <img
-          // src={urlFor(heroBanner.image)}
+          src={urlFor(heroBanner.image)}
           alt="headphones"
           className="hero-banner-image"
         />
 
         <div>
-          <Link
-            // href={`/product/${heroBanner.product}`}
-            href="/product/ID"
-          >
-            <button type="button">
-              {/* {heroBanner.buttonText} */} BUTTON TEXT
-            </button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>{/* {heroBanner.desc} */} DESCRIPTION</p>
+            <p>{heroBanner.desc} </p>
           </div>
         </div>
       </div>
