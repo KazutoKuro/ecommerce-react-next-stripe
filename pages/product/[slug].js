6dd261d63fd601113420c949e6614d1384d5fd6c
KazutoKuro <zinpaingsoe.dev@gmail.com>
2022-11-22 02:26:52 +0630

    Success page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -14,11 +14,11 @@ const ProductDetails = ({ product, products }) => {
   const [index, setIndex] = useState(0);
   const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
-  //   const handleBuyNow = () => {
-  //     onAdd(product, qty);
+  const handleBuyNow = () => {
+    onAdd(product, qty);
 
-  //     setShowCart(true);
-  //   };
+    setShowCart(true);
+  };
 
   return (
     <div>
@@ -80,11 +80,7 @@ const ProductDetails = ({ product, products }) => {
             >
               Add to Cart
             </button>
-            <button
-              type="button"
-              className="buy-now"
-              // onClick={handleBuyNow}
-            >
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
